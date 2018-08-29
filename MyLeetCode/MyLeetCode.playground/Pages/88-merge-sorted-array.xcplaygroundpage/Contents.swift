dd7e411f547cb8b1be8fb7e480ade538462537f7
Zhenjiang Liu <zliu@kpd-i.com>
2018-08-29 19:19:30 -0400

    88 merge 2 sorted array

---
+++ MyLeetCode/MyLeetCode.playground/Pages/88-merge-sorted-array.xcplaygroundpage/Contents.swift
@@ -0,0 +1,41 @@
+/*
+ Given two sorted integer arrays nums1 and nums2, merge nums2 into nums1 as one sorted array.
+ 
+ Note:
+ 
+ The number of elements initialized in nums1 and nums2 are m and n respectively.
+ You may assume that nums1 has enough space (size that is greater or equal to m + n) to hold additional elements from nums2.
+ Example:
+ 
+ Input:
+ nums1 = [1,2,3,0,0,0], m = 3
+ nums2 = [2,5,6],       n = 3
+ 
+ Output: [1,2,2,3,5,6]
+ */
+
+import Foundation
+
+class Solution {
+    // merge start from the biggest number
+    func merge(_ nums1: inout [Int], _ m: Int, _ nums2: [Int], _ n: Int) {
+        var m = m
+        var n = n
+        
+        while m > 0 && n > 0 {
+            if nums1[m - 1] > nums2[n - 1] {
+                nums1[m + n - 1] = nums1[m - 1]
+                m -= 1
+            } else {
+                nums1[m + n - 1] = nums2[n - 1]
+                n -= 1
+            }
+        }
+        
+        while n > 0 {
+            nums1[n - 1] = nums2[n - 1]
+            n -= 1
+        }
+    }
+}
+
