50790ba565856ce3d1567b469bbbdd92bffe71cb
Zhenjiang Liu <zliu@kpd-i.com>
2018-08-30 11:19:06 -0400

    104 maximum binary tree depth

---
+++ MyLeetCode/MyLeetCode.playground/Pages/104-maximum-tree-depth.xcplaygroundpage/Contents.swift
@@ -0,0 +1,46 @@
+/*
+ Given a binary tree, find its maximum depth.
+ 
+ The maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
+ 
+ Note: A leaf is a node with no children.
+ 
+ Example:
+ 
+ Given binary tree [3,9,20,null,null,15,7],
+ 
+  3
+ / \
+ 9  20
+   /  \
+  15   7
+ return its depth = 3.
+ */
+
+import Foundation
+
+class TreeNode {
+    var val: Int
+    var left: TreeNode?
+    var right: TreeNode?
+    
+    init(_ val: Int) {
+        self.val = val
+        self.left = nil
+        self.right = nil
+    }
+}
+
+class Solution {
+    func maxDepth(_ root: TreeNode?) -> Int {
+        guard root != nil else {
+            return 0
+        }
+        
+        guard root?.left != nil || root?.right != nil else {
+            return 1
+        }
+        
+        return 1 + max(maxDepth(root?.left), maxDepth(root?.right))
+    }
+}
