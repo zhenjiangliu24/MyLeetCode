d8f898d8dfd0d420d491be32d76b9357ce9b641c
Zhenjiang Liu <zliu@kpd-i.com>
2018-08-30 16:19:11 -0400

    107 binary tree level order traversal reverse

--- MyLeetCode/MyLeetCode.playground/Pages/107-binary-tree-level-order-traversal-reverse.xcplaygroundpage/Contents.swift
+++ MyLeetCode/MyLeetCode.playground/Pages/107-binary-tree-level-order-traversal-reverse.xcplaygroundpage/Contents.swift
@@ -1,5 +1,5 @@
 /*
- Given a binary tree, return the level order traversal of its nodes' values. (ie, from left to right, level by level).
+ Given a binary tree, return the bottom-up level order traversal of its nodes' values. (ie, from left to right, level by level from leaf to root).
  
  For example:
  Given binary tree [3,9,20,null,null,15,7],
@@ -8,13 +8,14 @@
  9  20
    /  \
   15   7
- return its level order traversal as:
+ return its bottom-up level order traversal as:
  [
- [3],
+ [15,7],
  [9,20],
- [15,7]
+ [3]
  ]
  */
+
 import Foundation
 
 class TreeNode {
@@ -30,7 +31,7 @@ class TreeNode {
 }
 
 class Solution {
-    func levelOrder(_ root: TreeNode?) -> [[Int]] {
+    func levelOrderBottom(_ root: TreeNode?) -> [[Int]] {
         var result = [[Int]]()
         var queue = [TreeNode]()
         
@@ -51,13 +52,14 @@ class Solution {
                 if let left = node.left {
                     queue.append(left)
                 }
+                
                 if let right = node.right {
                     queue.append(right)
                 }
             }
-            result.append(level)
+            
+            result.insert(level, at: 0)
         }
-        
         return result
     }
 }
